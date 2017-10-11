e388eb5b6a4fb9e78cc24e94b61cf46c3b5d82e5
Zachary Handshoe <32276520+ZacharyHandshoe@users.noreply.github.com>
2017-10-11 14:34:28 -0500

    Update On Array Sorter

--- src/sample/ArraySorter.java
+++ src/sample/ArraySorter.java
@@ -1,40 +1,72 @@
 package sample;
-import java.util.*;
 
 
+import java.util.Arrays;
 
 public class ArraySorter {
 
-    public String[] array = new String[10];
 
 
+    public ArraySorter(){
+
+    }
 
 
-    public static void main(String args[])
+    public static void main(String[] args)
     {
+        String[] foo = new String[10];
+        foo[0] = "Lenin, Vladimir dasklgh asdklhsdkla 124124";
+        foo[1] = "Stalin, Joseph dasklgh asdklh";
+        foo[2] = "Castro, Fidel dasklgh asdklh";
+        foo[3] = "REE, KREE dasklgh asdklh";
+        foo[4] = "Ww, ah dasklgh asdklh";
+        foo[5] = "As, Fas dasklgh asdklh";
+        foo[6] = "Ne, Ko dasklgh asdklh";
+        foo[7] = "Be, Bo dasklgh asdklh";
+        foo[8] = "He, Ho dasklgh asdklh";
+        foo[9] = "Yo, Bro dasklgh asdklh";
 
-
-
+        GetOrderedNames(foo);
+        PrintContent(foo);;
     }
 
-    @Override
-    public void start(){
-        array[0] = "Lenin, Vladimir";
-        array[1] = "Stalin, Joseph";
-        array[2] = "Castro, Fidel";
 
-        printContent();
-    }
-
-
-
-    public void printContent(){
-        for(int i = 0; i < array.length; i++)
+    public static void PrintContent(String[] nArray){
+        for(int i = 0; i < nArray.length; i++)
         {
-            System.out.println(array[i]);
+            System.out.println(nArray[i]);
         }
     }
 
+    public static String[] GetOrderedNames(String[] newArray)
+    {
+        Arrays.sort(newArray);
+
+        newArray = FirstLast(newArray);
+
+
+        return newArray;
+    }
+
+    public static String[] FirstLast(String[] fArray)
+    {
+        String[] finalArray = new String[fArray.length];
+
+        for(int i = 0; i < fArray.length; i++){
+            String toFlip = fArray[i];
+            int place = toFlip.indexOf(",");
+            String last = toFlip.substring(0, place - 1);
+
+            int stop = toFlip.indexOf(" ", place + 1);
+            String first = toFlip.substring(place + 1, stop);
+
+
+
+            finalArray[i] = first + ", " + last;
+
+        }
+        return finalArray;
+    }
 
 
 }
