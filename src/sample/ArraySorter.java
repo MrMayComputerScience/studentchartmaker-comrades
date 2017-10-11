2526f0f5e050c00e7742ad458c943c0d2f3b97a6
Zachary Handshoe <32276520+ZacharyHandshoe@users.noreply.github.com>
2017-10-11 13:31:48 -0500

    Progress 1

--- src/sample/ArraySorter.java
+++ src/sample/ArraySorter.java
@@ -5,14 +5,35 @@ import java.util.*;
 
 public class ArraySorter {
 
+    public String[] array = new String[10];
+
+
+
+
     public static void main(String args[])
     {
 
-        
+
 
     }
 
+    @Override
+    public void start(){
+        array[0] = "Lenin, Vladimir";
+        array[1] = "Stalin, Joseph";
+        array[2] = "Castro, Fidel";
 
+        printContent();
+    }
+
+
+
+    public void printContent(){
+        for(int i = 0; i < array.length; i++)
+        {
+            System.out.println(array[i]);
+        }
+    }
 
 
 
