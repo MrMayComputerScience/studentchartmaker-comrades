276e5987bcfed61401c993062f96324ee00e312d
Zach <edgleyshotfirst@users.noreply.github.com>
2017-10-19 14:33:42 -0500

    I did something useful
    hey you suck at this im a god haHAA

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,32 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.io.File;
 
+public class Main extends Application {
+    private Stage stage;
+    private File currentFile;
+    private ObservableList<String> tasks = FXCollections.observableArrayList();
+    @FXML
+    ListView<String> listView;
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Rick and Morty");
         primaryStage.setScene(new Scene(root, 602, 400));
         primaryStage.show();
+        stage = primaryStage;
     }
 
 
@@ -22,7 +35,13 @@ public class Main extends Application {
     }
     public void addThingIDontKnow()
     {
-        System.out.println("seize the means of production");
+        DirectoryChooser fileChooser = new DirectoryChooser();
+        fileChooser.setTitle("Open Resource File");
+        File f = fileChooser.showDialog(stage);
+        tasks.add(f.getPath());
+        listView.setItems(tasks);
+
+
     }
     public void removeThingIDontKnow()
     {
